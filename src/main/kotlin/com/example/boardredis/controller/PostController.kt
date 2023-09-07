ac4e0b2c931bb4a77e1b9fdc2deb00fffb90dfed
mck0324 <dch3030@naver.com>
2023-09-07 23:48:26 +0900

    GET Controller

--- src/main/kotlin/com/example/boardredis/controller/PostController.kt
+++ src/main/kotlin/com/example/boardredis/controller/PostController.kt
@@ -1,14 +1,17 @@
 package com.example.boardredis.controller
 
-import com.example.boardredis.controller.dto.PostCreateRequest
-import com.example.boardredis.controller.dto.PostUpdateRequest
+import com.example.boardredis.controller.dto.*
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 class PostController {
@@ -35,5 +38,20 @@ class PostController {
     ): Long{
         println(createdBy)
         return id
+    }
+
+    @GetMapping("/posts/{id}")
+    fun getPost(
+        @PathVariable id: Long,
+    ): PostDetailResponse{
+        return PostDetailResponse(1L,"title","content","createdBy",LocalDateTime.now().toString())
+    }
+
+    @GetMapping("/posts")
+    fun getPosts(
+        pageable: Pageable,
+        postSearchRequest: PostSearchRequest,
+    ): Page<PostSummaryResponse> {
+        return Page.empty()
     }
 }
