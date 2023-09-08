9e0aa83035cbe91fa709b190442ca65008f25625
mck0324 <dch3030@naver.com>
2023-09-08 23:46:04 +0900

    게시글 수정 서비스 작성

--- src/main/kotlin/com/example/boardredis/service/PostService.kt
+++ src/main/kotlin/com/example/boardredis/service/PostService.kt
@@ -1,8 +1,11 @@
 package com.example.boardredis.service
 
+import com.example.boardredis.exception.PostNotFoundException
 import com.example.boardredis.repository.PostRepository
 import com.example.boardredis.service.dto.PostCreateRequestDto
+import com.example.boardredis.service.dto.PostUpdateRequestDto
 import com.example.boardredis.service.dto.toEntity
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,5 +17,12 @@ class PostService(
     @Transactional
     fun createPost(requestDto: PostCreateRequestDto): Long {
         return postRepository.save(requestDto.toEntity()).id
+    }
+
+    @Transactional
+    fun updatePost(id: Long, requestDto: PostUpdateRequestDto): Long {
+        val post = postRepository.findByIdOrNull(id) ?: throw PostNotFoundException()
+        post.update(requestDto)
+        return id
     }
 }
