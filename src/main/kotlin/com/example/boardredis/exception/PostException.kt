b133ad28d9be7dc43b35f96e562a430e42bc056e
mck0324 <dch3030@naver.com>
2023-09-08 23:47:12 +0900

    게시글 수정 (DTO 및 예외처리)

---
+++ src/main/kotlin/com/example/boardredis/exception/PostException.kt
@@ -0,0 +1,7 @@
+package com.example.boardredis.exception
+
+open class PostException( message: String ) : RuntimeException(message)
+
+class PostNotFoundException(): PostException("게시글을 찾을 수 없습니다.")
+
+class PostNotUpdateableException(): PostException("게시글을 수정할 수 없습니다.")
