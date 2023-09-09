a5e57a09517618be26b6b5083fdccb3663837cef
mck0324 <dch3030@naver.com>
2023-09-09 17:43:30 +0900

    게시글 조회 ReponseDto

---
+++ src/main/kotlin/com/example/boardredis/service/dto/PostDetailResponseDto.kt
@@ -0,0 +1,19 @@
+package com.example.boardredis.service.dto
+
+import com.example.boardredis.domain.Post
+
+data class PostDetailResponseDto(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val createdBy: String,
+    val createdAt: String,
+)
+
+fun Post.toDetai성lResponseDto() = PostDetailResponseDto(
+    id = id,
+    title = title,
+    content = content,
+    createdBy = createdBy,
+    createdAt = createdAt.toString(),
+)
