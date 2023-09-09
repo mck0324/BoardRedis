1a570ac492c17e6d8649d5c0b3c6383ba648be14
mck0324 <dch3030@naver.com>
2023-09-10 01:15:24 +0900

    댓글 수정 (Controller)작성

--- src/main/kotlin/com/example/boardredis/controller/CommentController.kt
+++ src/main/kotlin/com/example/boardredis/controller/CommentController.kt
@@ -1,8 +1,10 @@
 package com.example.boardredis.controller
 
-import com.example.boardredis.controller.dto.CommentCreateReqeust
+import com.example.boardredis.controller.dto.CommentCreateRequest
+import com.example.boardredis.controller.dto.CommentUpdateRequest
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,10 +14,21 @@ class CommentController {
     @PostMapping("posts/{postId}/comments")
     fun createComment(
         @PathVariable postId: Long,
-        @RequestBody commentCreateRequest: CommentCreateReqeust,
+        @RequestBody commentCreateRequest: CommentCreateRequest,
     ): Long {
         println(commentCreateRequest.content)
         println(commentCreateRequest.createdBy)
         return 1L
     }
+
+    @PutMapping("comments/{commentId}")
+    fun updateComment(
+        @PathVariable commentId: Long,
+        @RequestBody commentUpdateRequest: CommentUpdateRequest,
+    ): Long {
+        println(commentUpdateRequest.content)
+        println(commentUpdateRequest.updatedBy)
+        return commentId
+    }
+
 }
