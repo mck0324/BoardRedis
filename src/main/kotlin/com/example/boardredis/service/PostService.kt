73c8e47a0d2c92b8b8faf5e974739d07ea5c38fc
mck0324 <dch3030@naver.com>
2023-09-09 17:42:39 +0900

    게시글 조회,목록조회 서비스작성

--- src/main/kotlin/com/example/boardredis/service/PostService.kt
+++ src/main/kotlin/com/example/boardredis/service/PostService.kt
@@ -1,11 +1,13 @@
 package com.example.boardredis.service
 
+import com.example.boardredis.service.dto.PostSummaryResponseDto
+import com.example.boardredis.service.dto.toSummaryResponseDto
 import com.example.boardredis.exception.PostNotDeleteableException
 import com.example.boardredis.exception.PostNotFoundException
 import com.example.boardredis.repository.PostRepository
-import com.example.boardredis.service.dto.PostCreateRequestDto
-import com.example.boardredis.service.dto.PostUpdateRequestDto
-import com.example.boardredis.service.dto.toEntity
+import com.example.boardredis.service.dto.*
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -33,5 +35,13 @@ class PostService(
         if (post.createdBy != deletedBy) throw PostNotDeleteableException()
         postRepository.delete(post)
         return id
+    }
+
+    fun getPost(id: Long): PostDetailResponseDto {
+        return postRepository.findByIdOrNull(id)?.toDetailResponseDto() ?: throw PostNotFoundException()
+    }
+
+    fun findPageBy(pageRequest: Pageable, postSearchRequestDto: PostSearchRequestDto): Page<PostSummaryResponseDto> {
+        return postRepository.findPageBy(pageRequest,postSearchRequestDto).toSummaryResponseDto()
     }
 }
