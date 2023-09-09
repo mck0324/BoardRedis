9f384d126557c7f2832922bd346c389def1faffd
mck0324 <dch3030@naver.com>
2023-09-10 01:46:16 +0900

    댓글 (Repository)

---
+++ src/main/kotlin/com/example/boardredis/repository/CommentRepository.kt
@@ -0,0 +1,7 @@
+package com.example.boardredis.repository
+
+import com.example.boardredis.domain.Comment
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CommentRepository: JpaRepository<Comment, Long> {
+}
