dc084b7220c5ff2883ad385ffa02c5f375c3f268
mck0324 <dch3030@naver.com>
2023-09-12 23:05:31 +0900

    댓글 Request.toDto()확장함수

--- src/main/kotlin/com/example/boardredis/controller/dto/CommentCreateRequest.kt
+++ src/main/kotlin/com/example/boardredis/controller/dto/CommentCreateRequest.kt
@@ -1,6 +1,13 @@
 package com.example.boardredis.controller.dto
 
+import com.example.boardredis.service.dto.CommentCreateRequestDto
+
 data class CommentCreateRequest(
     val content: String,
     val createdBy: String,
+)
+
+fun CommentCreateRequest.toDto() = CommentCreateRequestDto(
+    content = content,
+    createdBy = createdBy,
 )
--- src/main/kotlin/com/example/boardredis/controller/dto/CommentUpdateRequest.kt
+++ src/main/kotlin/com/example/boardredis/controller/dto/CommentUpdateRequest.kt
@@ -1,6 +1,13 @@
 package com.example.boardredis.controller.dto
 
+import com.example.boardredis.service.dto.CommentUpdateRequestDto
+
 data class CommentUpdateRequest(
     val content: String,
     val updatedBy: String,
+)
+
+fun CommentUpdateRequest.toDto() = CommentUpdateRequestDto(
+    content = content,
+    updatedBy = updatedBy,
 )
