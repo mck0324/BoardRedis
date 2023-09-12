341a6824c0ad8c9ad2dcc35462bbb7e0d8811e26
mck0324 <dch3030@naver.com>
2023-09-13 00:23:31 +0900

    댓글 조회 (게시글 조회 ResponseDto에 comments댓글추가)ResponseDto

---
+++ src/main/kotlin/com/example/boardredis/service/dto/CommentResponseDto.kt
@@ -0,0 +1,17 @@
+package com.example.boardredis.service.dto
+
+import com.example.boardredis.domain.Comment
+
+data class CommentResponseDto(
+    val id: Long,
+    val content: String,
+    val createdBy: String,
+    val createdAt: String,
+)
+
+fun Comment.toResponseDto() = CommentResponseDto(
+    id = id,
+    content = content,
+    createdBy = createdBy,
+    createdAt = createdAt.toString(),
+)
