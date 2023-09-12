b5582c04af60556b1f6797874581fcc98ff7f3b9
mck0324 <dch3030@naver.com>
2023-09-12 22:45:03 +0900

    댓글 수정 및 예외처리 Test

--- src/test/kotlin/com/example/boardredis/service/CommentServiceTest.kt
+++ src/test/kotlin/com/example/boardredis/service/CommentServiceTest.kt
@@ -2,10 +2,12 @@ package com.example.boardredis.service
 
 import com.example.boardredis.domain.Comment
 import com.example.boardredis.domain.Post
+import com.example.boardredis.exception.CommentNotUpdatableException
 import com.example.boardredis.exception.PostNotFoundException
 import com.example.boardredis.repository.CommentRepository
 import com.example.boardredis.repository.PostRepository
 import com.example.boardredis.service.dto.CommentCreateRequestDto
+import com.example.boardredis.service.dto.CommentUpdateRequestDto
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.comparables.shouldBeGreaterThan
@@ -55,11 +57,11 @@ class CommentServiceTest(
             content = "게시글 내용",
             createdBy = "게시글 생성자",
         ))
-        val saved = commentRepository.save(Comment("댓글내용",post,"댓글 생성자"))
+        val saved = commentRepository.save(Comment("댓글 내용",post,"댓글 생성자"))
         When("인풋이 정상적으로 들어오면") {
-            val updatedId = commentService.updateComment(1L,CommentUpdateRequestDto(
+            val updatedId = commentService.updateComment(saved.id, CommentUpdateRequestDto(
                 content = "수정된 댓글 내용",
-                updatedBy = "댓글 작성자"
+                updatedBy = "댓글 생성자"
             ))
             then("정상 수정됨을 확인") {
                 updatedId shouldBe saved.id
@@ -72,7 +74,7 @@ class CommentServiceTest(
         }
         When("작성자와 수정자가 다르면") {
             then("수정할 수 없는 게시물 예외가 발생") {
-                shouldThrow<CommentNotUpdateableException> { commentService.updateComment(saved.id, CommentUpdateRequestDto(
+                shouldThrow<CommentNotUpdatableException> { commentService.updateComment(saved.id, CommentUpdateRequestDto(
                     content = "수정된 댓글 내용",
                     updatedBy = "수정된 댓글 작성자"
                 )) }
