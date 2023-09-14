ffae3e5a5c86af1a74b61b6ed1c00cd298d52e63
mck0324 <dch3030@naver.com>
2023-09-15 00:02:31 +0900

    게시글 수정시 태그 수정 및 태그 순서 변경 되었을시 수정 Test

--- src/test/kotlin/com/example/boardredis/service/PostServiceTest.kt
+++ src/test/kotlin/com/example/boardredis/service/PostServiceTest.kt
@@ -80,7 +80,7 @@ class PostServiceTest(
         }
     }
     given("게시글 수정시") {
-        val saved = postRepository.save(Post(title = "title", content = "content", createdBy = "harris"))
+        val saved = postRepository.save(Post(title = "title", content = "content", createdBy = "harris", tags = listOf("tag1","tag2")))
         When("정상 수정시") {
             val updatedId = postService.updatePost(
                 saved.id,
@@ -125,6 +125,36 @@ class PostServiceTest(
                         )
                     )
                 }
+            }
+        }
+        When("태그가 수정되었을 때") {
+            val updatedId = postService.updatePost(
+                saved.id,
+                PostUpdateRequestDto(
+                    title = "update title",
+                    content = "update content",
+                    updatedBy = "harris",
+                    tags = listOf("tag1","tag2","tag3"),
+                )
+            )
+            then("정상적으로 수정됨을 확인") {
+                val tags = tagRepository.findByPostId(updatedId)
+                tags.size shouldBe 3
+                tags[2].name shouldBe "tag3"
+            }
+            then("태그 순서가 변경되었을때 정상적으로 변경됨을 확인") {
+                postService.updatePost(
+                    saved.id,
+                    PostUpdateRequestDto(
+                        title = "update title",
+                        content = "update content",
+                        updatedBy = "harris",
+                        tags = listOf("tags3","tags2","tag1"),
+                    )
+                )
+                val tags = tagRepository.findByPostId(updatedId)
+                tags.size shouldBe 3
+                tags[2].name shouldBe "tag1"
             }
         }
     }
