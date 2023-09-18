d8452552fedc00e7fbe516f008ab1c786afbaa19
mck0324 <dch3030@naver.com>
2023-09-18 23:34:41 +0900

    좋아요 (Repository 생성)

---
+++ src/main/kotlin/com/example/boardredis/repository/LikeRepository.kt
@@ -0,0 +1,7 @@
+package com.example.boardredis.repository
+
+import com.example.boardredis.domain.Like
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface LikeRepository: JpaRepository<Like, Long> {
+}
