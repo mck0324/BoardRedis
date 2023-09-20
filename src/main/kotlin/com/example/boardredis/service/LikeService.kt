f16e3b78fd5d0a358088fe84eaaa6894ae5ae350
mck0324 <dch3030@naver.com>
2023-09-20 23:12:49 +0900

    좋아요 Service

---
+++ src/main/kotlin/com/example/boardredis/service/LikeService.kt
@@ -0,0 +1,22 @@
+package com.example.boardredis.service
+
+import com.example.boardredis.domain.Like
+import com.example.boardredis.exception.PostNotFoundException
+import com.example.boardredis.repository.LikeRepository
+import com.example.boardredis.repository.PostRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class LikeService (
+    private val likeRepository: LikeRepository,
+    private val postRepository: PostRepository,
+) {
+    @Transactional
+    fun createLike(postId: Long, createdBy: String): Long {
+        val post = postRepository.findByIdOrNull(postId) ?: throw PostNotFoundException()
+        return likeRepository.save(Like(post, createdBy)).id
+    }
+}
