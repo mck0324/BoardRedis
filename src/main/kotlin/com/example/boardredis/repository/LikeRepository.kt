aa2fe051523c233a3e7300067ad86ec1cfb4646a
mck0324 <dch3030@naver.com>
2023-09-21 00:35:30 +0900

    좋아요 count개수 Repository

--- src/main/kotlin/com/example/boardredis/repository/LikeRepository.kt
+++ src/main/kotlin/com/example/boardredis/repository/LikeRepository.kt
@@ -3,4 +3,6 @@ package com.example.boardredis.repository
 import com.example.boardredis.domain.Like
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LikeRepository : JpaRepository<Like, Long>
+interface LikeRepository : JpaRepository<Like, Long> {
+    fun countByPostId(postId: Long): Long
+}
