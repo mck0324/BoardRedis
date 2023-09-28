335ed467bcfa3685e7807c9930554d314351edfb
mck0324 <dch3030@naver.com>
2023-09-28 23:35:10 +0900

    REDIS 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("com.h2database:h2")
